1cff78249b4176f88da57b88a3f8d33c10d5360b
efetekin481@outlook.com <efetekin481@outlook.com>
2023-03-21 22:42:33 +0300

    Add project files.

---
+++ constructorornek2/Kimlik.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace constructorornek2
+{
+    internal class Kimlik
+    {
+        string ad;
+        string soyad;
+        string memleket;
+        int yas;
+        char medenihal;
+
+       public string AD
+        {
+            get { return ad; }
+            set { ad = value.ToLower(); }
+        }
+
+       public string SOYAD
+        {
+            get { return soyad; }
+            set { soyad = value.ToUpper(); }
+        }   
+
+        public string MEMLEKET
+        {
+            get { return memleket; }
+            set { memleket = value; }
+        }
+
+        public int YAS
+        {
+            get { return yas; }
+            set { yas = Math.Abs(value); }
+        }
+
+        public char Medenihal
+        {
+            get { return medenihal; }
+            set { medenihal = value; }
+        }
+
+        public Kimlik()
+        {
+            ad = "";
+            soyad = "";
+            memleket = "";
+            yas = 0;
+            medenihal = 'B';
+        }
+    }
+}
---
+++ constructorornek2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace constructorornek2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Kimlik kml = new Kimlik();
+            Console.Write("Adınızı giriniz: ");
+            kml.AD = Console.ReadLine();
+            Console.Write("Soyadınızı giriniz: ");
+            kml.SOYAD= Console.ReadLine();
+            Console.Write("Memleketinizi giriniz: ");
+            kml.MEMLEKET=Console.ReadLine();
+            Console.Write("Yaşınızı sayı olarak giriniz: ");
+            kml.YAS=Convert.ToInt32(Console.ReadLine());
+            Console.Write("Medeni halinizi ''E'' veya ''B'' olarak giriniz: ");
+            kml.Medenihal=Convert.ToChar(Console.ReadLine());
+            Console.WriteLine(kml.AD);
+            Console.WriteLine(kml.SOYAD);
+            Console.WriteLine(kml.MEMLEKET);
+            Console.WriteLine(kml.YAS);
+            Console.WriteLine(kml.Medenihal);
+            Console.ReadLine();
+
+        }
+    }
+}
